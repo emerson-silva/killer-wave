ffbcbd5c2249c7479caa0ca39e5df1a499e179c9
emerson-silva <email.emerson.silva@gmail.com>
2023-05-20 11:46:46 -0300

    Initial Setup and Commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        CameraSetup();
+        SetupLight();
+    }
+
+    private void CameraSetup()
+    {
+        GameObject gameCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        // Camera Transform
+        gameCamera.transform.position = new Vector3(0, 0, -300);
+        gameCamera.transform.eulerAngles = new Vector3(0, 0, 0);
+
+        // Camera Properties
+        gameCamera.GetComponent<Camera>().clearFlags = CameraClearFlags.SolidColor;
+        gameCamera.GetComponent<Camera>().backgroundColor = new Color32(0,0,0,255);
+    }
+
+    private void SetupLight()
+    {
+        GameObject dirLight = GameObject.Find("Directional Light");
+        dirLight.transform.eulerAngles = new Vector3(50,-30,0);
+        dirLight.GetComponent<Light>().color = new Color32(152, 204, 255, 255);
+    }
+}
---
+++ Assets/Script/PlayerSpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    SOActorModel actorModel;
+    GameObject playerShip;
+
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreatePlayer()
+    {
+        //CREATE PLAYER
+        actorModel = Object.Instantiate(Resources.Load("Player_Default")) as SOActorModel;
+        playerShip = GameObject.Instantiate(actorModel.actor) as GameObject;
+        playerShip.GetComponent<Player>().ActorStats(actorModel);
+        //SET PLAYER UP
+        playerShip.transform.rotation = Quaternion.Euler(0, 180, 0);
+        playerShip.transform.localScale = new Vector3(60, 60, 60);
+        playerShip.name = "Player";
+        playerShip.transform.SetParent(this.transform);
+        playerShip.transform.position = Vector3.zero;
+    }
+}
